c2571889f2a889902782cbc36e2a0cf133658440
kokadva <kdvalishvili@opus2.com>
2020-06-02 16:39:40 +0400

    Refactor main file

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from OLA import slow_down
 from WavFile import WaveFile
 from utils import print_wave_file_header
 
-audio_file_path = 'taunt.wav'
-# audio_file_path = 'audio.wav'
+audio_file_path = 'audio-files/example.wav'
 wav_file = WaveFile(audio_file_path)
 print_wave_file_header(wav_file)
-
 slow_down(wav_file)
-# wav_file.sample_rate = int(wav_file.sample_rate * 1.5)
-wav_file.save('test.wav')
+wav_file.save('res.wav')
 
 
