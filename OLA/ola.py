3aeb32b2c00e8528e2598507c9f0f306e40f7c9b
kokadva <kdvalishvili@opus2.com>
2020-06-03 18:05:52 +0400

    Make overlap ration dependent on speed

--- OLA/ola.py
+++ OLA/ola.py
@@ -4,6 +4,8 @@ from OLA.constants import CHUNK_SIZE
 def set_speed(wav, speed=0.5):
     print("Slowing down by", speed)
 
+    chunk_overlap_ratio = 1 / speed / 4
+
     """
         If the we divied the whole audio file into constant size chunks and each chunk would be
         cut out from every N / 2 index then there will be File_Size / CHUNK_SIZE * 2 but it may give us an extra
@@ -14,11 +16,11 @@ def set_speed(wav, speed=0.5):
 
     """
         If you look closely after placing first chunk every next chunk adds samples number equal to the size of 
-        overlaping (size of overlaping would be CHUNK_SIZE * speed, size of the first chunk simply CHUNK_SIZE and 
-        the quantity of the other chunks besides the first chunks_num - 1 plus we use each one twise so we just 
-        multiply that value by two
+        overlaping (size of overlaping would be CHUNK_SIZE * chunk_overlap_ratio, size of the first chunk simply 
+        CHUNK_SIZE and the quantity of the other chunks besides the first chunks_num - 1 plus we use 
+        each one twise so we just multiply that value by two
     """
-    samples_num = int(CHUNK_SIZE + (2 * chunks_num - 1) * CHUNK_SIZE * speed)
+    samples_num = int(CHUNK_SIZE + (2 * chunks_num - 1) * CHUNK_SIZE * chunk_overlap_ratio)
 
     """
         Initialize the same strucute for the output as is for the input wav file (list consisting 
@@ -30,8 +32,8 @@ def set_speed(wav, speed=0.5):
 
     """
         When we add data to the output samples we must choose starting point for the chunk to overlap, the first value 
-        for it would be 0, then CHUNK_SIZE / SPEED, then 2 * (CHUNK_SIZE / SPEED) and on the X'th time it'll be 
-        X * (CHUNK_SIZE / SPEED) so this X will be our overlap_index
+        for it would be 0, then CHUNK_SIZE / chunk_overlap_ratio, then 2 * (CHUNK_SIZE / chunk_overlap_ratio) and on 
+        the X'th time it'll be X * (CHUNK_SIZE / chunk_overlap_ratio) so this X will be our overlap_index
     """
     overlap_index = 0
 
@@ -51,7 +53,7 @@ def set_speed(wav, speed=0.5):
 
                 for channel_index in range(wav.num_channels):
                     target_sample_index_to_copy = int((overlap_index + x)
-                                                      * CHUNK_SIZE * speed
+                                                      * CHUNK_SIZE * chunk_overlap_ratio
                                                       + (k - chunk_start_index))
                     output_samples[channel_index][target_sample_index_to_copy] = \
                         min(output_samples[channel_index][target_sample_index_to_copy] + wav.samples[channel_index][k],
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     output_audio_file_path = 'res.wav'
     wav_file = WaveFile(input_audio_file_path)
     print_wave_file_header(wav_file)
-    set_speed(wav_file)
+    set_speed(wav_file, 1.5)
     wav_file.save(output_audio_file_path)
